ef051c81a627e1a44e0657a4cf001b4ddf384ce8
EFSTRATIOU Angelos <aefstrat@netcompany.com>
2025-08-08 16:24:20 +0300

    Updates

---
+++ assets/maps/sprite_sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="sprite_sheet" tilewidth="32" tileheight="32" spacing="2" tilecount="12" columns="12">
+ <image source="../pngs/sprite_sheet.png" width="419" height="34"/>
+</tileset>
